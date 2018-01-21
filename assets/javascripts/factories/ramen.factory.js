5281c976778472f143023d11a2874619603ff282
Jana Veliskova <jveliskova@rjmetrics.com>
2018-01-21 11:43:20 -0500

    add ramen factory

---
+++ assets/javascripts/factories/ramen.factory.js
@@ -0,0 +1,13 @@
+(function () {
+    'use strict';
+    angular
+        .module('app')
+        .factory('Ramen', Ramen);
+
+    Ramen.$inject = ['$http'];
+
+    function Ramen ($http) {
+        
+    }
+
+})();
